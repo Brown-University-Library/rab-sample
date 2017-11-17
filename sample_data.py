7557185cba63aab85d94b32d94672e80e4085bf9
Steven McCauley <steven_mccauley@brown.edu>
2017-11-17 16:28:16 -0500

    Process for bgenberg successfully terminates

--- sample_data.py
+++ sample_data.py
@@ -15,90 +15,134 @@ import requests
 import config.development as config
 
 faculty = [
-    'http://vivo.brown.edu/individual/khansenm',
-    'http://vivo.brown.edu/individual/embrown',
-    'http://vivo.brown.edu/individual/jpipher',
-    'http://vivo.brown.edu/individual/lbrownmd',
-    'http://vivo.brown.edu/individual/etollmd',
-    'http://vivo.brown.edu/individual/jvandenb',
-    'http://vivo.brown.edu/individual/bgenberg',
-    'http://vivo.brown.edu/individual/mhohenha',
-    'http://vivo.brown.edu/individual/eterrymo',
-    'http://vivo.brown.edu/individual/aplette',
-    'http://vivo.brown.edu/individual/rarenber',
-    'http://vivo.brown.edu/individual/cfaulkne',
-    'http://vivo.brown.edu/individual/mklitzke',
-    'http://vivo.brown.edu/individual/jdibened',
-    'http://vivo.brown.edu/individual/cvillarr',
-    'http://vivo.brown.edu/individual/scohenmd',
-    'http://vivo.brown.edu/individual/mhillstr',
-    'http://vivo.brown.edu/individual/mdasilva',
-    'http://vivo.brown.edu/individual/cjsammar',
-    'http://vivo.brown.edu/individual/lsnadymc',
-    'http://vivo.brown.edu/individual/pruberto',
-    'http://vivo.brown.edu/individual/jkuzner',
-    'http://vivo.brown.edu/individual/pharropm',
-    'http://vivo.brown.edu/individual/ss74',
-    'http://vivo.brown.edu/individual/cn8',
-    'http://vivo.brown.edu/individual/rbhatt',
-    'http://vivo.brown.edu/individual/dnickel',
-    'http://vivo.brown.edu/individual/awebb',
-    'http://vivo.brown.edu/individual/efwalsh',
-    'http://vivo.brown.edu/individual/ll7',
-    'http://vivo.brown.edu/individual/kmonchik',
-    'http://vivo.brown.edu/individual/ewittels',
-    'http://vivo.brown.edu/individual/egartman',
-    'http://vivo.brown.edu/individual/ljrubinm',
-    'http://vivo.brown.edu/individual/ffenghi',
-    'http://vivo.brown.edu/individual/rpadilla',
-    'http://vivo.brown.edu/individual/elaposat',
-    'http://vivo.brown.edu/individual/ccarpent',
-    'http://vivo.brown.edu/individual/rbungiro',
-    'http://vivo.brown.edu/individual/sschenna',
-    'http://vivo.brown.edu/individual/wc53',
-    'http://vivo.brown.edu/individual/ac184',
-    'http://vivo.brown.edu/individual/mapomera',
-    'http://vivo.brown.edu/individual/gdk',
-    'http://vivo.brown.edu/individual/mfaganmd',
-    'http://vivo.brown.edu/individual/nlawandy',
-    'http://vivo.brown.edu/individual/dchronle',
-    'http://vivo.brown.edu/individual/rwestlak',
-    'http://vivo.brown.edu/individual/rpendse',
-    'http://vivo.brown.edu/individual/nkouttab'
+    # 'http://vivo.brown.edu/individual/khansenm',
+    # 'http://vivo.brown.edu/individual/embrown',
+    # 'http://vivo.brown.edu/individual/jpipher',
+    # 'http://vivo.brown.edu/individual/lbrownmd',
+    # 'http://vivo.brown.edu/individual/etollmd',
+    # 'http://vivo.brown.edu/individual/jvandenb',
+    # 'http://vivo.brown.edu/individual/bgenberg',
+    # 'http://vivo.brown.edu/individual/mhohenha',
+    # 'http://vivo.brown.edu/individual/eterrymo',
+    # 'http://vivo.brown.edu/individual/aplette',
+    # 'http://vivo.brown.edu/individual/rarenber',
+    # 'http://vivo.brown.edu/individual/cfaulkne',
+    # 'http://vivo.brown.edu/individual/mklitzke',
+    # 'http://vivo.brown.edu/individual/jdibened',
+    # 'http://vivo.brown.edu/individual/cvillarr',
+    # 'http://vivo.brown.edu/individual/scohenmd',
+    # 'http://vivo.brown.edu/individual/mhillstr',
+    # 'http://vivo.brown.edu/individual/mdasilva',
+    # 'http://vivo.brown.edu/individual/cjsammar',
+    # 'http://vivo.brown.edu/individual/lsnadymc',
+    # 'http://vivo.brown.edu/individual/pruberto',
+    # 'http://vivo.brown.edu/individual/jkuzner',
+    # 'http://vivo.brown.edu/individual/pharropm',
+    # 'http://vivo.brown.edu/individual/ss74',
+    # 'http://vivo.brown.edu/individual/cn8',
+    # 'http://vivo.brown.edu/individual/rbhatt',
+    # 'http://vivo.brown.edu/individual/dnickel',
+    # 'http://vivo.brown.edu/individual/awebb',
+    # 'http://vivo.brown.edu/individual/efwalsh',
+    # 'http://vivo.brown.edu/individual/ll7',
+    # 'http://vivo.brown.edu/individual/kmonchik',
+    # 'http://vivo.brown.edu/individual/ewittels',
+    # 'http://vivo.brown.edu/individual/egartman',
+    # 'http://vivo.brown.edu/individual/ljrubinm',
+    # 'http://vivo.brown.edu/individual/ffenghi',
+    # 'http://vivo.brown.edu/individual/rpadilla',
+    # 'http://vivo.brown.edu/individual/elaposat',
+    # 'http://vivo.brown.edu/individual/ccarpent',
+    # 'http://vivo.brown.edu/individual/rbungiro',
+    # 'http://vivo.brown.edu/individual/sschenna',
+    # 'http://vivo.brown.edu/individual/wc53',
+    # 'http://vivo.brown.edu/individual/ac184',
+    # 'http://vivo.brown.edu/individual/mapomera',
+    # 'http://vivo.brown.edu/individual/gdk',
+    # 'http://vivo.brown.edu/individual/mfaganmd',
+    # 'http://vivo.brown.edu/individual/nlawandy',
+    # 'http://vivo.brown.edu/individual/dchronle',
+    # 'http://vivo.brown.edu/individual/rwestlak',
+    # 'http://vivo.brown.edu/individual/rpendse',
+    'http://vivo.brown.edu/individual/bgenberg'
 ]
 
-# orgs = [
-#     'http://vivo.brown.edu/individual/org-brown-univ-dept70',
-#     'http://vivo.brown.edu/individual/org-brown-univ-dept602',
-#     'http://vivo.brown.edu/individual/org-brown-univ-dept604',
-#     'http://vivo.brown.edu/individual/org-brown-univ-dept6',
-#     'http://vivo.brown.edu/individual/org-brown-univ-dept29',
-#     'http://vivo.brown.edu/individual/org-brown-univ-dept140',
-#     'http://vivo.brown.edu/individual/org-brown-univ-dept254',
-#     'http://vivo.brown.edu/individual/org-brown-univ-dept362',
-#     'http://vivo.brown.edu/individual/org-brown-univ-dept13',
-#     'http://vivo.brown.edu/individual/org-brown-univ-dept24',
-#     'http://vivo.brown.edu/individual/org-brown-univ-dept221',
-#     'http://vivo.brown.edu/individual/org-brown-univ-dept263',
-#     'http://vivo.brown.edu/individual/org-brown-univ-dept20',
-#     'http://vivo.brown.edu/individual/org-brown-univ-dept266',
-#     'http://vivo.brown.edu/individual/org-brown-univ-dept231',
-#     'http://vivo.brown.edu/individual/org-brown-univ-dept15',
-#     'http://vivo.brown.edu/individual/org-brown-univ-dept42',
-#     'http://vivo.brown.edu/individual/org-brown-univ-dept41',
-#     'http://vivo.brown.edu/individual/org-brown-univ-dept31',
-#     'http://vivo.brown.edu/individual/org-brown-univ-dept14'
-# ]
 
-def traverse(nodes):
-    data = query(nodes.pop())
-    filtered = filterData(data)
-    nbors = pathfinder(filtered)
-    nodes = nodesnbors
+def traverse(nodes, visited=[]):
+    node = nodes.pop()
+    data = query(node)
+    parsed = parseResponse(data)
+    filtered = filterData(parsed)
+    nbors = pathFinder(filtered)
+    nodes = nodes + nbors
     if not nodes:
         return filtered
     else:
         return filtered + traverse(nodes)
+
+def filterData(data):
+    strip_properties =  [
+            '<http://vivoweb.org/ontology/core#organizationForPosition>',
+            '<http://vivoweb.org/ontology/core#organizationForTraining>',
+            '<http://vivoweb.org/ontology/core#hasCollaborator>',
+            '<http://vivo.brown.edu/ontology/vivo-brown/hasAffiliation>',
+            '<http://vivoweb.org/ontology/core#researchAreaOf>',
+            '<http://vivo.brown.edu/ontology/citation#hasContributor>',
+            '<http://vivo.brown.edu/ontology/citation#venueFor>',
+            '<http://vivo.brown.edu/ontology/profile#trainingFor>',
+            '<http://vivo.brown.edu/ontology/vivo-brown/cvOf>',
+            '<http://vivoweb.org/ontology/core#webpageOf>',
+            '<http://vivoweb.org/ontology/core#educationalTrainingOf>',
+            '<http://vivoweb.org/ontology/core#positionForPerson>',
+            '<http://vivoweb.org/ontology/core#subOrganizationWithin>',
+            '<http://vivo.brown.edu/ontology/display#BrownOrg>'
+        ]
+    # strip_properties = {
+    #     '<http://xmlns.com/foaf/0.1/Organization>' : [
+    #         '<http://vivoweb.org/ontology/core#organizationForPosition>',
+    #         '<http://vivoweb.org/ontology/core#organizationForTraining>'
+    #     ],
+    #     '<http://vivoweb.org/ontology/core#FacultyMember>' : [
+    #         '<http://vivoweb.org/ontology/core#hasCollaborator>',
+    #         '<http://vivo.brown.edu/ontology/vivo-brown/hasAffiliation>'
+    #     ],
+    #     '<http://www.w3.org/2004/02/skos/core#Concept>' : [
+    #         '<http://vivoweb.org/ontology/core#researchAreaOf>'
+    #     ],
+    #     '<http://vivo.brown.edu/ontology/citation#Citation>' : [
+    #         '<http://vivo.brown.edu/ontology/citation#hasContributor>'
+    #     ],
+    #     '<http://vivo.brown.edu/ontology/citation#Venue>' : [
+    #         '<http://vivo.brown.edu/ontology/citation#venueFor>'
+    #     ],
+    #     '<http://vivo.brown.edu/ontology/profile#PostdocAppointment>' : [
+    #         '<http://vivo.brown.edu/ontology/profile#trainingFor>'
+    #     ],
+    #     '<http://vivoweb.org/ontology/core#URLLink>' : [
+    #         '<http://vivo.brown.edu/ontology/vivo-brown/cvOf>',
+    #         '<http://vivoweb.org/ontology/core#webpageOf>'
+    #     ],
+    #     '<http://vivoweb.org/ontology/core#EducationalTraining>': [
+    #         '<http://vivoweb.org/ontology/core#educationalTrainingOf>'
+    #     ],
+    #     '<http://vivoweb.org/ontology/core#FacultyPosition>' : [
+    #         '<http://vivoweb.org/ontology/core#positionForPerson>'
+    #     ]
+    # }
+    # type_data = [ t for t in data 
+    #                 if t[1] == '<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>' and
+    #                     t[2] in strip_properties.keys() ]
+    # if len(type_data) == 0:
+    #     return data
+    # rmv = strip_properties[type_data[0][2]]
+    return [ t for t in data if t[1] not in strip_properties ]
+
+def pathFinder(data):
+    skip = [ '<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>',
+            '<http://vitro.mannlib.cornell.edu/ns/vitro/0.7#mostSpecificType>',
+            '<http://vitro.mannlib.cornell.edu/ns/vitro/public#mostSpecificType>' ]
+    return [ t[2][1:-1] for t in data if t[2].startswith('<') and
+                t[1] not in skip ]
 
 def parse_ntriple(row):
     s = row[:row.index(' ')]
@@ -106,18 +150,15 @@ def parse_ntriple(row):
     p = row[:row.index(' ')]
     row = row[row.index(' ') + 1:]
     o = row[:row.rindex(" .")]
-    # o = row[:]
     return (s,p,o)
 
-# def split_ntriples(nt):
-#     delim = ' .\n'
-#     line_end = nt.index(delim)
-#     idx = nt.index(delim)
-#     parsed = nt[:idx]
-#     if nt[idx + len(delim):] == '\n':
-#         return [ parsed ]
-#     else:
-#         return [parsed ] + split_ntriples(nt[idx + len(delim):])
+def parseResponse(resp):
+    if resp == '':
+        return []
+    triples = resp.splitlines()
+    if triples[-1] == '':
+        triples = triples[:-1]
+    return [ parse_ntriple(t) for t in triples ]
 
 def query(uri):
     query = """
@@ -128,17 +169,17 @@ def query(uri):
     data = { 'email': config.email, 'password': config.passw, 'query': query }
     resp = requests.post(config.query_url, data=data, headers=headers)
     if resp.status_code == 200:
-        triples = resp.text.splitlines()
-        return [ parse_ntriple(t) for t in triples ]
+        return resp.text
     else:
-        return []
+        return ''
 
 if __name__ == '__main__':
-    shortid = sys.argv[1]
-    print query('http://vivo.brown.edu/individual/' + shortid)
-    # with open('data/genberg.nt','r') as f:
+    # with open('data/genberg.nt', 'r') as f:
     #     nt = f.read()
-    # for line in nt.splitlines():
-    #     if line == '\n':
-    #         break
-    #     print parse_ntriple(line)
+    # parsed = parseResponse(nt)
+    # filtered = filterData(parsed)
+    # print pathFinder(filtered)
+    out = traverse(faculty)
+    subs = { o[0] for o in out }
+    for s in subs:
+        print s
